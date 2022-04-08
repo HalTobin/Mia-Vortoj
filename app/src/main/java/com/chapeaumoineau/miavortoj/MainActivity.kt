a7d0236507e8dd70089bf705910c342fdbba4f88
Alexis Aneas <altair.alek@gmail.com>
2022-04-08 17:07:38 +0200

    update and refactor

--- app/src/main/java/com/chapeaumoineau/miavortoj/MainActivity.kt
+++ app/src/main/java/com/chapeaumoineau/miavortoj/MainActivity.kt
@@ -1,14 +1,9 @@
-package com.plcoding.cleanarchitecturenoteapp
+package com.chapeaumoineau.miavortoj
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.plcoding.cleanarchitecturenoteapp.ui.theme.CleanArchitectureNoteAppTheme
+import com.chapeaumoineau.miavortoj.ui.theme.CleanArchitectureNoteAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/test/java/com/chapeaumoineau/miavortoj/ExampleUnitTest.kt
+++ app/src/test/java/com/chapeaumoineau/miavortoj/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.plcoding.cleanarchitecturenoteapp
+package com.chapeaumoineau.miavortoj
 
 import org.junit.Test
 
